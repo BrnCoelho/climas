8187a5a0b5146ba375400189efd9b0a9c41e1264
BiaCodess <pereiracoelhobeatriz@gmail.com>
2025-07-09 21:19:43 -0300

    Add files via upload

---
+++ Rust/target/debug/build/proc-macro2-8ecdc549423c9909/build_script_build-8ecdc549423c9909.d
@@ -0,0 +1,5 @@
+C:\Users\perei\Documents\RustCodes\Projeto\projeto4\target\debug\build\proc-macro2-8ecdc549423c9909\build_script_build-8ecdc549423c9909.exe: C:\Users\perei\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.95\build.rs
+
+C:\Users\perei\Documents\RustCodes\Projeto\projeto4\target\debug\build\proc-macro2-8ecdc549423c9909\build_script_build-8ecdc549423c9909.d: C:\Users\perei\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.95\build.rs
+
+C:\Users\perei\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.95\build.rs:
---
+++ Rust/target/debug/projeto4.d
@@ -0,0 +1,1 @@
+C:\Users\perei\Documents\RustCodes\Projeto\projeto4\target\debug\projeto4.exe: C:\Users\perei\Documents\RustCodes\Projeto\projeto4\src\main.rs
